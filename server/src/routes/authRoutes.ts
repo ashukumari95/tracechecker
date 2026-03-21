a7fa559ce59952d7a44a5371993b715d73107311
ashukumari95 <aashukumari3311@gmail.com>
2026-03-21 10:44:21 +0530

    chore: clean push without node_modules

--- server/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { adminLogin } from '../controllers/authController';
-
-const router = express.Router();
-
-// Path: /api/v1/auth/admin-login
-router.post('/admin-login', adminLogin);
-
-export default router;
